c913be3c6d14fa43caab93fed2f58f3ecf16c7ae
Chad Ross <rosschad98@icloud.com>
2021-11-08 14:37:35 -0600

    Create REST API serializers

---
+++ music_controller/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from rest_framework.utils import field_mapping
+from .models import Room
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
